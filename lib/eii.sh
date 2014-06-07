1e2ed0bc14d8a7c916de29ee336facc277ef8824
Kevin Vollmer <works.kvollmer@gmail.com>
2014-06-06 20:56:52 -0400

    Collecting options split from operational logic

--- lib/eii.sh
+++ lib/eii.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-usage()
-{
-  echo "usage: $0 [-s <table> <title> <year> <author>]" 1>&2;
-  echo
-  echo "  s : select all matching entries"
-  echo "  u : update all matching entries"
-  echo "  d : delete all matching entries"
-  echo "  i : insert new entries"
-  echo "  l : list tables"
-  exit 1;
-}
-
-title=
-year=
-author=
-
-reader()
-{
-  ary=($@)
-  title=${ary[0]}
-  year=${ary[1]}
-  author=${ary[2]}
-}
+#━━━━━━━━━━━━━━━━━━━━━━━(Functions)━━━━━━━━━━━━━━━━━━━━━━━━━
+usage() { cat usage; }
 
 read_s_args()
 {
-  while (($#)) && [[ $1 != -* ]]
+  if [ -z "$2" ]
+  then
+    echo $1: argument required.
+    exit
+  fi
+  local var="$1"
+  while (($#)) && [[ $2 != -* ]]
   do
-    sargs+=("$1")
+    eval "$var+=("$2")"
     shift
   done
 }
@@ -36,9 +21,8 @@ read_s_args()
 exec_sql()
 {
   db=../bin/eib.db
-  dbrc=eiirc
   touch $db
-  res=$(sqlite3 --init $dbrc --batch $db "$*")
+  res=$(sqlite3 --batch $db "$*")
   echo "$res" | less -R
 }
 
@@ -61,13 +45,12 @@ sql_fields()
 {
   sql="PRAGMA table_info($1);"
   query_sql "$sql"
-  fields=$(echo "$res" | cut -d '|' -f2)
+  echo $(echo "$res" | cut -d '|' -f2)
 }
 
 sql_select()
 {
-  sql="SELECT * FROM $1 WHERE $2 = $3;"
-  echo "$sql"
+  echo "SELECT $1 FROM $2 WHERE $3 = $4;"
 }
 
 sql_insert()
@@ -75,47 +58,75 @@ sql_insert()
   if [ -z $4 ]; then ver='1'; else ver=$4; fi;
     sql="INSERT INTO master VALUES
     ( null, '$1', '$2', '$3', '$ver');"
-  }
-
-  if [ -z $1 ]
+}
+#━━━━━━━━━━━━━━━━━━━━━━━━(Options)━━━━━━━━━━━━━━━━━━━━━━━━━━
+while (($#)); do
+  case "$1" in
+    -h|--help)
+      usage
+      exit
+      ;;
+    -s|--select)
+      option="-s"
+      ;;
+    -i|--insert)
+      read_s_args ${@}
+      if [ ${#sargs[@]} -eq 4 -o 5 ]
+      then
+        reader ${sargs[@]:1}
+        sql_insert ${sargs[@]:1}
+        exec_sql $sql
+      else
+        echo "wrong number of args.
+        Expecting [4||5], got ${#sargs[@]}."
+        usage
+      fi
+      ;;
+    -u|--update)
+      arg=${OPTARG}
+      echo $arg
+      ;;
+    -d|--delete)
+      arg=${OPTARG}
+      echo $arg
+      ;;
+    -l|--list)
+      echo "||||||||||||"
+      ;;
+#━━━━━━━━━━━━━━━━━━━━━━━(Selectors)━━━━━━━━━━━━━━━━━━━━━━━━━
+    -t|--table)
+      read_s_args tables ${@:2}
+      ;;
+    -c|--column)
+      read_s_args columns ${@:2}
+      ;;
+    -f|--filter)
+      read_s_args filters ${@:2}
+      ;;
+    -v|--values)
+      read_s_args values ${@:2}
+      ;;
+  esac
+  shift
+done
+#━━━━━━━━━━━━━━━━━━━━━━━━━━(Main)━━━━━━━━━━━━━━━━━━━━━━━━━━━
+if [ -z $option ]
+then
+  exit
+elif [ $option == "-s" ]
+then
+  echo _____run_____
+  set -f
+  if [ -z $tables ];
   then
-    usage
+    sql_tables
   fi
 
-  while (($#)); do
-    case "$1" in
-      -h|--help)
-        usage
-        ;;
-      -s|--select)
-        read_s_args ${@}
-        sql_select ${@:2}
-        exec_sql "$sql"
-        ;;
-      -i|--insert)
-        read_s_args ${@}
-        if [ ${#sargs[@]} -eq 4 -o 5 ]
-        then
-          reader ${sargs[@]:1}
-          sql_insert ${sargs[@]:1}
-          exec_sql $sql
-        else
-          echo "wrong number of args.
-          Expecting [4||5], got ${#sargs[@]}."
-          usage
-        fi
-        ;;
-      -u|--update)
-        arg=${OPTARG}
-        echo $arg
-        ;;
-      -d|--delete)
-        arg=${OPTARG}
-        echo $arg
-        ;;
-      -l|--list)
-        echo "||||||||||||"
-        ;;
-    esac
-    shift
-  done
+  if [ -z $columns ]; then columns="*"; fi
+  if [ -z $filters ]; then filters="id"; fi
+  echo tables:  ${tables[@]}
+  echo columns: ${columns[@]}
+  echo filters: ${filters[@]}
+  echo values:  ${values[@]}
+  exec_sql $(sql_select $columns $tables $filters $values) 
+fi
