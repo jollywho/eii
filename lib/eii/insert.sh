a72616a083eb13bf59b9d0bfc1e1244b7c0254ea
Kevin Vollmer <works.kvollmer@gmail.com>
2014-06-09 00:20:38 -0400

    Insert script updated

--- lib/eii/insert.sh
+++ lib/eii/insert.sh
@@ -3,17 +3,19 @@ touch $db
 
 exec_sql()
 {
+  echo $@
   sqlite3 --batch $db "$*"
 }
 
 gen_vals()
 {
-  for i in {1..50}
+  for i in $(seq 1 $2)
   do
-    exec_sql "INSERT INTO $1 VALUES (null,2,3,4,5);"
+    exec_sql "INSERT INTO $1 VALUES (null,'"name_$i"','"$i"',4,5);"
   done
 }
 
-gen_vals book
-gen_vals anime
-gen_vals movie
+gen_vals book 5
+gen_vals anime 10
+gen_vals movie 3
+gen_vals tv 7
