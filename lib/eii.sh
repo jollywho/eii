f9f8317085532000022f8bb843b3b1e26f2f1a72
Kevin Vollmer <works.kvollmer@gmail.com>
2014-06-10 17:09:13 -0400

    Table loop

--- lib/eii.sh
+++ lib/eii.sh
@@ -15,40 +15,40 @@ then
   echo _____run_____
   set -f
 
+  # generate tables if none given
   if [ -z "$tables" ];
   then
-    echo gen tables
     table_data
   fi
 
-   table_data
-  #todo: loop tables
+  for table in ${tables[@]}
+  do
+    filters=$(echo ${filters[@]} | tr " " ",")
+    columns=$(echo ${columns[@]} | tr " " ",")
 
-  filters=$(echo ${filters[@]} | tr " " ",")
-  columns=$(echo ${columns[@]} | tr " " ",")
-
-  # has no columns && has values
-  if [ -z "$columns" ] && [ -n "$values" ]
-  then
-    # generate columns for table
-    columns=$(eval echo "$"$m"" | tr " " ",")
-    m=$(echo "${tables[1]}") #single table for debug
-    concat_sql or $columns $values}
-    #filters=$(concat_sql ${fields[@]} ${values[@]} or)
-  fi
-
-  # has filters
-  if [ -n "$filters" ]
-  then
-    # has no columns
-    if [ -n "$columns" ]
+    # has no columns && has values
+    if [ -z "$columns" ] && [ -n "$values" ]
     then
-      columns=$(echo ${filters[@]} | tr " " ",")
+      # generate columns for table
+      columns=$(eval echo "$"$m"" | tr " " ",")
+      m=$(echo "$table") #single table for debug
+      concat_sql or $columns $values}
+      #filters=$(concat_sql ${fields[@]} ${values[@]} or)
     fi
-  fi
 
-  # if nothing above set columns, use wildchar
-  if [ -z $columns ]; then columns="*"; fi
+    # has filters
+    if [ -n "$filters" ]
+    then
+      # has no columns
+      if [ -n "$columns" ]
+      then
+        columns=$(echo ${filters[@]} | tr " " ",")
+      fi
+    fi
 
-  exec_sql $(sql_select $columns ${tables[1]} "$filters")
+    # if nothing above set columns, use wildchar
+    if [ -z $columns ]; then columns="*"; fi
+
+    exec_sql $(sql_select $columns $table "$filters")
+  done
 fi
