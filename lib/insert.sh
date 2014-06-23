6f6e979adf31b87f0406364d89f2a6e1afa643fe
Kevin Vollmer <works.kvollmer@gmail.com>
2014-06-22 23:07:35 -0400

    check if enough values supplied to insert without error

--- lib/insert.sh
+++ lib/insert.sh
@@ -5,8 +5,19 @@ run_insert()
     exit
   fi
 
-   fields=$(sql_fields $table)
-   s_name=$(echo ${fields[@]})
+  # count # of fields
+  fields=$(sql_fields $table)
+  s_name=$(echo ${fields[@]})
+  count=$(echo $s_name | tr -cd , | wc -c)
+  c_count=$((count+1))
+
+  v_count=($s_value)
+  v_count=${#v_count[@]}
+
+  # if enough values supplied for existing fields
+  if [ $c_count -eq $v_count ]; then
+    echo ok
+  fi
 }
 
 sql_insert()
