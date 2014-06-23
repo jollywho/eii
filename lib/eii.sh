a6edf552309fbaccef2101d09a83e13a0a7cd326
Kevin Vollmer <works.kvollmer@gmail.com>
2014-06-22 22:39:55 -0400

    exits

--- lib/eii.sh
+++ lib/eii.sh
@@ -2,38 +2,40 @@
 source functions.sh
 source options.sh
 source select.sh
+source insert.sh
 #━━━━━━━━━━━━━━━━━━━━━━━━━(Main)━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 run "$@"
 
 #Disable file name generation using metacharacters
 set -f
 
-if [ -n $option ]; then
-  # generate tables if none supplied
-  if [ -z "$tables" ]; then
-    table_data
-  fi
-
-  for table in ${tables[@]}
-  do
-    s_filter=$(echo ${filters[@]} | tr " " ",")
-    s_column=$(echo ${columns[@]} | tr " " ",")
-    s_value=$(echo -e ${values[@]})
-
-    if [ $option == "-s" ]; then
-      run_select
-    elif [ $option == "-i" ]; then
-      # eii -i -t book -v j j j
-      # field names for table
-      # need values; exit without
-      # compare column with values
-      echo
-    elif [ $option == "-u" ]; then
-      echo
-    elif [ $option == "-d" ]; then
-      # eii -d -t book | -c name | -v name_02
-      # need filters and values; exit without
-      echo
-    fi
-  done
+# exit if no option set
+if [ -z $option ]; then
+  exit
 fi
+# generate tables if none supplied
+if [ -z "$tables" ]; then
+  table_data
+fi
+
+for table in ${tables[@]}
+do
+  s_filter=$(echo ${filters[@]} | tr " " ",")
+  s_column=$(echo ${columns[@]} | tr " " ",")
+  s_value=$(echo -e ${values[@]})
+
+  if [ $option == "-s" ]; then
+    run_select
+  elif [ $option == "-i" ]; then
+    # eii -i -t book -v j j j
+    # field names for table
+    # compare column with values
+    run_insert
+  elif [ $option == "-u" ]; then
+    echo
+  elif [ $option == "-d" ]; then
+    # eii -d -t book | -c name | -v name_02
+    # need filters and values; exit without
+    echo
+  fi
+done
--- lib/insert.sh
+++ lib/insert.sh
@@ -1,9 +1,15 @@
 run_insert()
 {
-  
+  # exit if no values supplied
+  if [ -z "$s_value" ]; then
+    exit
+  fi
+
+   fields=$(sql_fields $table)
+   s_name=$(echo ${fields[@]})
 }
 
 sql_insert()
 {
-    echo "INSERT INTO $1 VALUES ( $2 );"
+   echo "INSERT INTO $1 VALUES ( $2 );"
 }
